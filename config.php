d922674263eaa7ae1756055f904716f1adc709dd
TheNatrix01 <64905896+TheNatrix01@users.noreply.github.com>
2022-07-28 14:51:04 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'abysal');
+define('DB_PASSWORD', 'q6SF&6aW6gFb');
+define('DB_NAME', 'controlpanel');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
